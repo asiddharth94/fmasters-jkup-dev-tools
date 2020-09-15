ba47ded9e3954298632f469c3ace93be5dec78f9
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2020-09-16 00:19:27 +0530

    Exercise-5

--- public/js/mark-and-measure.js
+++ public/js/mark-and-measure.js
@@ -1,9 +1,19 @@
 window.addEventListener("load", thingToMeasure);
 
 function thingToMeasure() {
+  // const start = new Date().getTime();
+  performance.mark('start');
   fetch("/api")
-    .then(result => result.json())
-    .then(json => {
-      json.images.forEach(image => console.log(image.name));
-    });
+  .then(result => result.json())
+  .then(json => {
+    json.images.forEach(image => console.log(image.name));
+    // const end = new Date().getTime();
+    performance.mark('end');
+    // const result = end - start;
+    performance.measure('Our measurment', 'start', 'end');
+    // console.log(result);
+    const measurment = performance.getEntriesByType('measure');
+    console.log(measurment);
+  });
 }
+
